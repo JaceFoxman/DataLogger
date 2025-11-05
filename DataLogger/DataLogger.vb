37c8fff8fd6bb6baa9447f6c63d2f73642f995e3
Jason Permann <JaceFoxman@gmail.com>
2025-11-05 11:11:38 -0700

    Add project files.

---
+++ DataLogger/DataLogger.vb
@@ -0,0 +1,15 @@
+﻿'Jason Permann
+'Fall 2025
+'Jason Permann
+'RCET 3371
+'Data Logger Application
+'
+
+Option Strict On
+Option Explicit On
+Option Compare Text
+Public Class DataLogger
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
